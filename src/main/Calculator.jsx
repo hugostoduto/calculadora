a22236cadf75ff9629acc6f950c1d1c674ff20f6
Hugo Stoduto <hugoarstoduto@gmail.com>
2022-02-11 22:00:04 -0300

    Inicio do projeto Calculadora

---
+++ src/main/Calculator.jsx
@@ -0,0 +1,9 @@
+import React, { component } from "react";
+import { Component } from "react/cjs/react.development";
+import "./Calculator.css";
+
+export default class Calculator extends Component {
+  render() {
+    return <div className="calculator"></div>;
+  }
+}
