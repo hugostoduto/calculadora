a7c878554368aa3bc2435feb65757cfa05b7056e
Hugo Stoduto <hugoarstoduto@gmail.com>
2022-02-11 22:49:58 -0300

    Criacao do componente <Button/>

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -1,9 +1,30 @@
-import React, { component } from "react";
+import React from "react";
 import { Component } from "react/cjs/react.development";
+import { Button } from "../components/Button";
 import "./Calculator.css";
 
 export default class Calculator extends Component {
   render() {
-    return <div className="calculator"></div>;
+    return (
+      <div className="calculator">
+        <Button label="ac" />
+        <Button label="/" />
+        <Button label="7" />
+        <Button label="8" />
+        <Button label="9" />
+        <Button label="*" />
+        <Button label="4" />
+        <Button label="5" />
+        <Button label="6" />
+        <Button label="-" />
+        <Button label="1" />
+        <Button label="2" />
+        <Button label="3" />
+        <Button label="+" />
+        <Button label="0" />
+        <Button label="." />
+        <Button label="=" />
+      </div>
+    );
   }
 }
